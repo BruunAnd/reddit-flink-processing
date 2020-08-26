817ff7daaa8a85801303359bd0adb8fe0d6e8175
Anders Jakobsen <andersjkbsn@gmail.com>
2020-08-26 20:51:29 +0200

    Use event time processing

--- commentprocessing/src/main/java/commentprocessing/CommentProcessingJob.java
+++ commentprocessing/src/main/java/commentprocessing/CommentProcessingJob.java
@@ -1,18 +1,23 @@
 package commentprocessing;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.formats.json.JsonNodeDeserializationSchema;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
+import java.time.Duration;
 import java.util.Properties;
 
 public class CommentProcessingJob {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "localhost:9092");
@@ -22,9 +27,15 @@ public class CommentProcessingJob {
                 .map(CommentProcessingJob::mapToComment)
                 .name("comments");
 
+        // Assign timestamps, multiplied by 1K to get milliseconds
+        WatermarkStrategy<RedditComment> strategy = WatermarkStrategy
+                .<RedditComment>forBoundedOutOfOrderness(Duration.ofSeconds(30))
+                .withTimestampAssigner((comment, time) -> comment.getTime() * 1000);
+
         DataStream<String> counts = comments
+                .assignTimestampsAndWatermarks(strategy)
                 .keyBy(RedditComment::getSubreddit)
-                .window(TumblingProcessingTimeWindows.of(Time.minutes(1)))
+                .window(TumblingEventTimeWindows.of(Time.minutes(1)))
                 .process(new KeywordCountProcessFunction())
                 .name("comment-counter");
 
